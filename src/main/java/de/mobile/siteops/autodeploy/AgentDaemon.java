f765de9721f40986740c81f9a4f60453fba6a9f2
Jonathan Colby <jcolby@ebay.de>
2012-09-17 19:53:32 +0200

    check for null env variable USE_FQDN and whether it is set to TRUE

--- src/main/java/de/mobile/siteops/autodeploy/AgentDaemon.java
+++ src/main/java/de/mobile/siteops/autodeploy/AgentDaemon.java
@@ -11,7 +11,7 @@ public class AgentDaemon {
 
     private static Logger logger = Logger.getLogger(AgentDaemon.class.getName());
 
-    public static final String VERSION = "0.9.9";
+    public static final String VERSION = "0.9.9a";
     
     private static AgentJob getAgentJob(String[] args) throws ConfigurationInvalidException {
         Configuration config = new Configuration();
--- src/main/java/de/mobile/siteops/autodeploy/AgentUtils.java
+++ src/main/java/de/mobile/siteops/autodeploy/AgentUtils.java
@@ -69,7 +69,7 @@ public final class AgentUtils {
         String fullyQualifiedHostName = System.getenv("USE_FQDN");
         String hostName = null;
 
-        if (fullyQualifiedHostName.toUpperCase().equals("TRUE")) {
+        if (fullyQualifiedHostName != null && fullyQualifiedHostName.toUpperCase().equals("TRUE")) {
             hostName = address.getCanonicalHostName();
             logger.info("fully qualified hostname is '" + hostName + "' and environment is '" + environment + "'");
         } else {
